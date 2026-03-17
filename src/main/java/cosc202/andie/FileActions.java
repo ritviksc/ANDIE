95a182ceba5156d70b9b6369b6b26a79c354de09
loekvanbroekhoven <loekvanbroekhoven@mac.byod.uod.otago.ac.nz>
2026-03-17 13:29:04 +1300

    export fix

--- src/main/java/cosc202/andie/FileActions.java
+++ src/main/java/cosc202/andie/FileActions.java
@@ -43,6 +43,7 @@ public class FileActions {
         actions.add(new FileOpenAction("Open", null, "Open a file", KeyEvent.VK_O));
         actions.add(new FileSaveAction("Save", null, "Save the file", KeyEvent.VK_S));
         actions.add(new FileSaveAsAction("Save As", null, "Save a copy", KeyEvent.VK_A));
+        actions.add(new FileExportAction("Export", null, "Export image", KeyEvent.VK_A));
         actions.add(new FileExitAction("Exit", null, "Exit the program", 0));
     }
 
@@ -218,52 +219,7 @@ public class FileActions {
         }
 
     }
-    
-        public class FileSaveAsAction extends ImageAction {
-
-        /**
-         * <p>
-         * Create a new file-save-as action.
-         * </p>
-         *
-         * @param name The name of the action (ignored if null).
-         * @param icon An icon to use to represent the action (ignored if null).
-         * @param desc A brief description of the action (ignored if null).
-         * @param mnemonic A mnemonic key to use as a shortcut (ignored if
-         * null).
-         */
-        FileSaveAsAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
-            super(name, icon, desc, mnemonic);
-        }
-
-        /**
-         * <p>
-         * Callback for when the file-save-as action is triggered.
-         * </p>
-         *
-         * <p>
-         * This method is called whenever the FileSaveAsAction is triggered. It
-         * prompts the user to select a file and saves the image to it.
-         * </p>
-         *
-         * @param e The event triggering this callback.
-         */
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            JFileChooser fileChooser = new JFileChooser();
-            int result = fileChooser.showSaveDialog(target);
-
-            if (result == JFileChooser.APPROVE_OPTION) {
-                try {
-                    String imageFilepath = fileChooser.getSelectedFile().getCanonicalPath();
-                    target.getImage().saveAs(imageFilepath);
-                } catch (Exception ex) {
-                    System.exit(1);
-                }
-            }
-        }
-
-    }
+   
         
     public class FileExportAction extends ImageAction {
 

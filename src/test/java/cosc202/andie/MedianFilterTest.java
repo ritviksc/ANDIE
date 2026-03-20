cc4496d7e23f3c1e6f01160072dca209e47936b6
rober <rober@Borigmalil.mshome.net>
2026-03-20 18:11:08 +1300

    Fixed comment

---
+++ src/test/java/cosc202/andie/MedianFilterTest.java
@@ -0,0 +1,26 @@
+
+package cosc202.andie;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Robert Hannaford
+ */
+public class MedianFilterTest {
+    
+    public MedianFilterTest() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+    
+}

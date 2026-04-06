2814e1f4f11cee7362d80ff4192af88072f5b988
loekvanbroekhoven <loekvanbroekhoven@mac.byod.uod.otago.ac.nz>
2026-04-06 19:54:09 +1200

    added menu and toolbar setup

--- src/main/java/cosc202/andie/Andie.java
+++ src/main/java/cosc202/andie/Andie.java
@@ -2,6 +2,8 @@ package cosc202.andie;
 
 import static cosc202.andie.ImageAction.target;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.FileInputStream;
@@ -36,6 +38,8 @@ import java.util.Properties;
  * @version 1.0
  */
 public class Andie {
+    
+    private static JButton activeButton;
 
     /**
      * <p>
@@ -52,8 +56,6 @@ public class Andie {
      *
      * @see ImagePanel
      * @see ImageAction
-
-
     * @see ImageOperation
      * @see FileActions
      * @see EditActions
@@ -63,6 +65,7 @@ public class Andie {
      *
      * @throws Exception if something goes wrong.
      */
+    
     private static void createAndShowGUI() throws Exception {
         // Set up the main GUI frame
         JFrame frame = new JFrame("ANDIE");
@@ -77,32 +80,104 @@ public class Andie {
         JScrollPane scrollPane = new JScrollPane(imagePanel);
         frame.add(scrollPane, BorderLayout.CENTER);
 
-        // Add in menus for various types of action the user may perform.
-        JMenuBar menuBar = new JMenuBar();
-
+        //create the top row for all the main actions headers
+        JPanel mainRow = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        mainRow.setBackground(new Color(255, 255, 255));
         // File menus are pretty standard, so things that usually go in File menus go here.
         FileActions fileActions = new FileActions();
-        menuBar.add(fileActions.createMenu());
-
+        JButton fileButton = new JButton(I18nManager.get("file_Title"));
+        mainRow.add(fileButton); 
         // Likewise Edit menus are very common, so should be clear what might go here.
         EditActions editActions = new EditActions();
-        menuBar.add(editActions.createMenu());
-
+        JButton editButton = new JButton(I18nManager.get("Edit_title"));
+        mainRow.add(editButton);
         // View actions control how the image is displayed, but do not alter its actual content
         ViewActions viewActions = new ViewActions();
-        menuBar.add(viewActions.createMenu());
-
+        JButton viewButton = new JButton(I18nManager.get("View_title"));
+        mainRow.add(viewButton);
         // Filters apply a per-pixel operation to the image, generally based on a local window
-        FilterActions filterActions = new FilterActions();
-        menuBar.add(filterActions.createMenu());
-
+        FilterActions filterActions = new FilterActions(); 
+        JButton filterButton = new JButton(I18nManager.get("Filter_title"));
+        mainRow.add(filterButton);
         // Actions that affect the representation of colour in the image
         ColourActions colourActions = new ColourActions();
-        menuBar.add(colourActions.createMenu());
-
+        JButton colourButton = new JButton(I18nManager.get("colour_title"));
+        mainRow.add(colourButton);
         // Language action controls language of the app
-        SettingsActions languageActions = new SettingsActions();
-        menuBar.add(languageActions.createMenu());
+        SettingsActions languageActions = new SettingsActions();  
+        JButton settingsButton = new JButton(I18nManager.get("Setting_title"));
+        mainRow.add(settingsButton);
+        
+        
+        
+           
+        //aesthetic fix on the buttons
+        styleMenuButton(fileButton);
+        styleMenuButton(editButton);
+        styleMenuButton(viewButton);
+        styleMenuButton(filterButton);
+        styleMenuButton(colourButton);
+        styleMenuButton(settingsButton);
+        
+        JPanel toolbarPanel = new JPanel(new CardLayout());
+        toolbarPanel.setBackground(new Color(255, 255, 255));
+        toolbarPanel.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.BLACK));
+        toolbarPanel.setVisible(false);
+        
+        //add functions to toolbar
+        toolbarPanel.add(editActions.createToolBar(), "EDIT");
+        toolbarPanel.add(viewActions.createToolBar(), "VIEW");
+        toolbarPanel.add(colourActions.createToolBar(), "COLOUR");
+             
+        CardLayout cardLayout = (CardLayout) toolbarPanel.getLayout();
+        
+        editButton.addActionListener((ActionEvent e) -> {
+            cardLayout.show(toolbarPanel, "EDIT");
+            setActiveButton(editButton);
+            toolbarPanel.setVisible(true);
+        });
+
+        viewButton.addActionListener((ActionEvent e) -> {
+            cardLayout.show(toolbarPanel, "VIEW");
+            setActiveButton(viewButton);
+            toolbarPanel.setVisible(true);
+        });
+
+        colourButton.addActionListener((ActionEvent e) -> {
+            cardLayout.show(toolbarPanel, "COLOUR");
+            setActiveButton(colourButton);
+            toolbarPanel.setVisible(true);
+        });
+        
+        //drop down menu for file
+        JMenu fileMenu = fileActions.createMenu();
+        JPopupMenu filePopup = fileMenu.getPopupMenu();
+
+        fileButton.addActionListener(e -> {
+            filePopup.show(fileButton, 0, fileButton.getHeight());
+        });
+        //drop down menu for filter
+        JMenu filterMenu = filterActions.createMenu();
+        JPopupMenu filterPopup = filterMenu.getPopupMenu();
+        
+        filterButton.addActionListener( e -> {
+                filterPopup.show(filterButton, 0, filterButton.getHeight());
+        });
+        //drop down menu for settings
+        JMenu settingsMenu = languageActions.createMenu();
+        JPopupMenu settingsPopup = settingsMenu.getPopupMenu();
+
+        settingsButton.addActionListener(e -> {
+            settingsPopup.show(settingsButton, 0, settingsButton.getHeight());
+        });
+
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
+
+        topPanel.add(mainRow);
+        topPanel.add(toolbarPanel);
+
+        frame.add(topPanel, BorderLayout.NORTH);
 
         frame.addWindowListener(new WindowAdapter() {
             @Override
@@ -169,9 +244,49 @@ public class Andie {
             }
         });
 
-        frame.setJMenuBar(menuBar);
+
         frame.pack();
         frame.setVisible(true);
+    }
+    /**makes some aesthetic changes to the main menu button, change colour when the mouse hovers over
+     * @param button 
+     */ 
+    private static void styleMenuButton(JButton button) {
+        Color normalColour = Color.WHITE;
+        Color hoverColour = new Color(240, 240, 240);
+
+        button.setFocusPainted(false);
+        button.setBorderPainted(false);
+        button.setContentAreaFilled(false);
+        button.setOpaque(true);
+        button.setBackground(normalColour);
+        button.setMargin(new Insets(4, 12, 4, 12));
+        button.setFont(new Font("SansSerif", Font.BOLD, 14));
+
+        button.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent e) {
+                if (button != activeButton) {
+                    button.setBackground(hoverColour);
+                }
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent e) {
+                if (button != activeButton) {
+                    button.setBackground(normalColour);
+                }
+            }
+        });
+    }
+    //chnages the aestheics of the button that is currerntly selects also changes back the previously selected menu button
+    private static void setActiveButton(JButton button) {
+        if (activeButton != null) {
+            activeButton.setBackground(Color.WHITE);
+        }
+        
+        button.setBackground(new Color(240, 240, 240));
+        activeButton = button;
     }
 
     /**

65d8a2f15e19d6abeff1b014593c207c35f05f99
loekvanbroekhoven <loekvanbroekhoven@mac.byod.uod.otago.ac.nz>
2026-04-13 01:38:50 +1200

    editing MacroManger

--- src/main/java/cosc202/andie/MacroManger.java
+++ src/main/java/cosc202/andie/MacroManger.java
@@ -5,7 +5,9 @@
 package cosc202.andie;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 /**
@@ -15,7 +17,9 @@ import java.util.ArrayList;
 public class MacroManger {
     private boolean recording = false;
     private ArrayList<ImageOperation> operationsList = new ArrayList<>();
-    
+    /**
+     * starts macro and clears previous operationsList
+     */
     public void start(){
         recording = true;
         operationsList.clear();
@@ -24,8 +28,9 @@ public class MacroManger {
      * stops the recording of the macro and saves the file to a marco if there are recrod operations
      * @param file the file to save the macro to
      * @return true if macro is saved, false if there where no operations and macro wasn't saved
+     * @throws java.io.IOException if save(file) fails
      */
-    public boolean stop(File file){
+    public boolean stop(File file) throws IOException{
         recording = false;
         
         if(operationsList.isEmpty()){
@@ -41,11 +46,17 @@ public class MacroManger {
             operationsList.add(op);
         }
     }
-    
-    public void save(File file){
-        try{
-        } catch(IOException){
-            System.out.println("can't save macro file");
+    /**
+     * saves macro to file
+     * @param file  
+     * @throws IOException 
+     */
+    public void save(File file) throws IOException{
+        try (
+            FileOutputStream fileOut = new FileOutputStream(file);
+            ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+        ){
+            objOut.writeObject(operationsList);
         }
     }
     

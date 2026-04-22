c0e16ebb09eec063c748b8dcc7250de610d828b2
malee <malee@10.47.63.38>
2026-04-22 14:10:09 +1200

    Fixed ImageThesholding

--- src/main/java/cosc202/andie/ColourActions.java
+++ src/main/java/cosc202/andie/ColourActions.java
@@ -191,7 +191,7 @@ public class ColourActions extends ToolbarActions {
                 return;
             }
 
-            // target.getImage().apply(new ConvertToGrey());
+            target.getImage().apply(new ConvertToGrey());
             target.getImage().apply(new ImageThresholdingFilter(threshold));
             target.repaint();
             target.getParent().revalidate();

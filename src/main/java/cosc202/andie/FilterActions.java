99c251eaf7713101151cf78c412ff95eed268f9c
rober <rober@Borigmalil.mshome.net>
2026-04-29 14:39:57 +1200

    FIR filter now uses new image

--- src/main/java/cosc202/andie/FilterActions.java
+++ src/main/java/cosc202/andie/FilterActions.java
@@ -39,7 +39,7 @@ public class FilterActions extends ToolbarActions {
         actions.add(new MedianFilterAction(I18nManager.get("Median"), new ImageIcon(Andie.class.getClassLoader().getResource("ToolbarIcons/Filters/Median.png")), I18nManager.get("Median_desc"), KeyEvent.VK_D));
         actions.add(new EmbossFilterAction(I18nManager.get("Emboss"), new ImageIcon(Andie.class.getClassLoader().getResource("ToolbarIcons/Filters/Emboss.png")), I18nManager.get("Emboss_desc"), KeyEvent.VK_E));
         actions.add(new SobelFilterAction(I18nManager.get("Sobel"), new ImageIcon(Andie.class.getClassLoader().getResource("ToolbarIcons/Filters/Sobel.png")), I18nManager.get("Sobel_desc"), KeyEvent.VK_O));
-        actions.add(new FIRFilterAction(I18nManager.get("FIR"), new ImageIcon(Andie.class.getClassLoader().getResource("ToolbarIcons/Filters/randomScattering.png")), I18nManager.get("FIR_desc"), KeyEvent.VK_F));
+        actions.add(new FIRFilterAction(I18nManager.get("FIR"), new ImageIcon(Andie.class.getClassLoader().getResource("ToolbarIcons/Filters/FIR.png")), I18nManager.get("FIR_desc"), KeyEvent.VK_F));
         actions.add(new ContrastMaskAction(I18nManager.get("Contrast_mask"), new ImageIcon(Andie.class.getClassLoader().getResource("ToolbarIcons/Filters/contrastMask.png")), I18nManager.get("Contrast_mask_desc"), KeyEvent.VK_C));
         actions.add(new RandomScatteringAction(I18nManager.get("random_scattering"), new ImageIcon(Andie.class.getClassLoader().getResource("ToolbarIcons/Filters/randomScattering.png")), I18nManager.get("random_scattering_desc"), KeyEvent.VK_R));
         

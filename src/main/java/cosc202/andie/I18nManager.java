b8b1a991b72116db317a19b846e7350bf959a44d
Ritvik Sharma <shari838@student.otago.ac.nz>
2026-05-05 13:24:27 +1200

    Updated documentation for some classes.

--- src/main/java/cosc202/andie/I18nManager.java
+++ src/main/java/cosc202/andie/I18nManager.java
@@ -2,9 +2,17 @@ package cosc202.andie;
 
 import java.util.*;
 
+/**
+ * I18N Manager for ANDIE to manage Locale and ResourceBundles
+ * @author shari838
+ */
 public class I18nManager {
     private static ResourceBundle bundle;
 
+    /**
+     * Set language locale for current selected language
+     * @param locale 
+     */
     public static void init(Locale locale) {
         if (locale == null) {
             locale = Locale.ENGLISH;  // Default to English
@@ -12,6 +20,11 @@ public class I18nManager {
         bundle = ResourceBundle.getBundle("messages", locale);
     }
 
+    /**
+     * Return translation of sentence
+     * @param key
+     * @return 
+     */
     public static String get(String key) {
         return bundle.getString(key);
     }
